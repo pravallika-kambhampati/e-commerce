a5612cffb0c1c2752665f21266c3e3b573313010
Pravallika Kambhampati <pravallika@upcalibre.com>
2021-06-09 15:14:00 +0530

    day-2

--- market/routes.py
+++ market/routes.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template
-import os
-app = Flask(__name__)
+from market import app
+from flask import render_template
+from market.models import Item
 
 @app.route('/')
 @app.route('/home')
@@ -14,9 +14,5 @@ def home_page():
 
 @app.route('/market')
 def market_page():
-    items = [
-    {'id': 1, 'name': 'Phone', 'barcode': '893212299897', 'price': 500},
-    {'id': 2, 'name': 'Laptop', 'barcode': '123985473165', 'price': 900},
-    {'id': 3, 'name': 'Keyboard', 'barcode': '231985128446', 'price': 150}
-    ]
+    items = Item.query.all()
     return render_template('market.html', items = items)
