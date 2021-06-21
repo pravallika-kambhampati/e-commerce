4c781a81f152959c75d2c7438706b9463dfd2943
Pravallika Kambhampati <pravallika@upcalibre.com>
2021-06-21 20:59:25 +0530

    day-5

--- market/routes.py
+++ market/routes.py
@@ -3,6 +3,7 @@ from flask import render_template, redirect, url_for, flash, get_flashed_message
 from market.models import Item, User
 from market.forms import RegisterForm, LoginForm
 from market import db
+from flask_login import login_user, logout_user, login_required
 
 @app.route('/')
 @app.route('/home')
@@ -15,6 +16,7 @@ def home_page():
 #     return f'<h1>This is the about page of {username}</h1>'
 
 @app.route('/market')
+@login_required
 def market_page():
     items = Item.query.all()
     return render_template('market.html', items = items)
@@ -27,7 +29,9 @@ def register_page():
                               email_addr=form.email_address.data,
                               password=form.password1.data)
         db.session.add(user_to_create)
-        db.session.commit()
+        db.session.commit() 
+        login_user(user_to_create)
+        flash(f'Account created successfully! You are now logged in as {user_to_create.username}', category='success')
         return redirect(url_for('market_page'))
     if form.errors != {}: #if there are no errors from the validations 
         for err_msg in form.errors.values():
@@ -38,5 +42,19 @@ def register_page():
 @app.route('/login', methods=['GET','POST'])
 def login_page():
     form = LoginForm()
-
+    if form.validate_on_submit():
+        attempted_user = User.query.filter_by(username=form.username.data).first()
+        if attempted_user and attempted_user.check_password_correction(attempted_password=form.password.data):
+            login_user(attempted_user)
+            flash(f'Success! You are logged in as: {attempted_user.username} :)', category='success')
+            return redirect(url_for('market_page'))
+        else:
+            flash('Username and password are not matched! Pls try again :(',category='danger')
     return render_template('login.html', form = form)
+
+
+@app.route('/logout')
+def logout_page():
+    logout_user()
+    flash(f'You have been logged out!', category='info')
+    return redirect(url_for("home_page"))
